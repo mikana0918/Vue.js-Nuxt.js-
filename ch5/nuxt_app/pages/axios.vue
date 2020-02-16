2fcc52823cb31e6fff92da78782c54da12ee3a61
Apple <shogo.kusuhara@gmail.com>
2020-02-16 21:24:32 +0900

    by p.400

--- ch5/nuxt_app/pages/axios.vue
+++ ch5/nuxt_app/pages/axios.vue
@@ -2,8 +2,30 @@
     <section class="container">
         <h1>{{title}}</h1>
         <p>{{message}}</p>
-        <ul v-for="(data, key) in json_data">
-            <li>{{data.name}} ({{data.age}}) [{{key}}]</li>
+        <table>
+            <tr>
+                <th>Email</th>
+                <td><input v-model="email"></td>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td><input v-model="username"></td>
+            </tr>
+            <tr>
+                <th>Age</th>
+                <td><input type="number" v-model="age"></td>
+            </tr>
+            <tr>
+                <th>Tel</th>
+                <td><input v-model="tel"></td>
+            </tr>
+            <tr><th></th><td>
+                <button @click="addData">Click</button>
+            </td></tr>
+        </table>
+        <hr>
+        <ul v-for="(data, key) in json_data" v-bind:key="data.id">
+            <li><strong>{{key}}</strong><br>{{data}}</li>
         </ul>
     </section>
 </template>
@@ -11,19 +33,50 @@
 <script>
 const axios = require('axios');
 
-let url = "https://my-new-nuxt-app.firebaseio.com/person.json";
+let url = "https://my-new-nuxt-app.firebaseio.com/person";
 
 export default {
-    data: function(){
+    data: function() {
         return {
             title:'Axios',
             message: 'Axios sample.',
+            json_data: {},
+            username: '',
+            tel: '',
+            age:0,
+            email: ''
         };
     },
-    asyncData: async function() {
-        let result = await axios.get(url);
-        return {json_data: result.data};
-    }
+    methods: {
+        //inputから受け取ったデータをFirebaseへ反映させる
+        addData: function() {
+            let add_url = url + '/' + this.email + '.json';
+            let data = {
+                'name' : this.username,
+                'age' : this.age,
+                'tel' : this.tel
+            };
+            axios.put(add_url, data).then((re) => {
+                this.email = '';
+                this.username = '';
+                this.age = 0;
+                this.tel = '';
+                this.getData();
+            });
+        },
+        getData: function() {
+            axios.get(url + '.json').then((res) => {
+                this.message = 'get all data.';
+                this.json_data = res.data;
+            }).catch((error) => {
+                this.message = 'ERROR!';
+                this.json_data = {};
+            });
+        },
+        created: function() {
+            this.getData();
+        }
+    },
 }
 </script>
 
