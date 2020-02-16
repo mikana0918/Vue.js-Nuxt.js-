0b20509876fa5c8603a2c991b5c9221d702a32a9
Apple <shogo.kusuhara@gmail.com>
2020-02-16 13:57:00 +0900

    by p.383

--- ch5/nuxt_app/pages/axios.vue
+++ ch5/nuxt_app/pages/axios.vue
@@ -2,51 +2,44 @@
     <section class="container">
         <h1>{{title}}</h1>
         <p>{{message}}</p>
-        <table>
-            <tr>
-                <th>User ID</th>
-                <td>{{json_data.userId}}</td>
-            </tr>
-            <tr>
-                <th>ID</th>
-                <td>{{json_data.id}}</td>
-            </tr>
-            <tr>
-                <th>Title</th>
-                <td>{{json_data.title}}</td>
-            </tr>
-            <tr>
-                <th>Body</th>
-                <td>{{json_data.body}}</td>
-            </tr>
-        </table>
+        <ul v-for="(data, key) in json_data">
+            <li>{{data.name}} ({{data.age}}) [{{key}}]</li>
+        </ul>
     </section>
 </template>
 
 <script>
 const axios = require('axios');
 
-let url = "https://jsonplaceholder.typicode.com/posts/";
+let url = "https://my-new-nuxt-app.firebaseio.com/person.json";
 
 export default {
     data: function(){
         return {
             title:'Axios',
-            msg: '',
-            message: 'Axios sample.'
+            message: 'Axios sample.',
         };
     },
-    //asyncDataに非同期通信によるDataをストックする
-    //html_dataはそのまま、dataへマージされる
-    asyncData: async function () {
-        let id = 1;// id番号
-        let result = await axios.get(url + id);
+    asyncData: async function() {
+        let result = await axios.get(url);
         return {json_data: result.data};
     }
 }
 </script>
 
 <style>
+ul {
+    margin: 0px 10px;
+    background-color: aliceblue;
+}
+li {
+    padding: 10px;
+    font-size: 16px;
+}
+input, button {
+    font-size:14px;
+}
+
 tr th {
     width: 150px;
     background-color: darkblue;
